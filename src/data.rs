e6a1f5d3571cfa630e550811b6cdbe64e0e22d41
bugsnotabunny <66078473+bugsnotabunny@users.noreply.github.com>
2023-01-03 09:24:24 +0300

    first steps

---
+++ src/data.rs
@@ -0,0 +1,13 @@
+use std::LinkedList;
+
+struct Data {
+    users: LinkedList< User >,
+    groups: LinkedList< Group >
+}
+
+impl Data {
+    //! fn create_group(...);
+    //! fn register_user(...);
+    //! fn delete_group(...);
+    //! fn delete_user(...);
+}
