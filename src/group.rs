a95b158baa3ef1dc2809315947f96785b7b76a53
bugsnotabunny <66078473+bugsnotabunny@users.noreply.github.com>
2023-01-03 15:53:33 +0300

    create ultra basic user and group classes

--- src/group.rs
+++ src/group.rs
@@ -1,1 +1,11 @@
-struct Group;
+// use serde::{Deserialize, Serialize};
+// use serde_json::Result;
+
+// #[derive(Serialize, Deserialize)]
+pub struct Group {
+
+}
+
+impl Group {
+
+}
--- src/user.rs
+++ src/user.rs
@@ -1,1 +1,13 @@
-struct User;
+// use serde::{Deserialize, Serialize};
+// use serde_json::Result;
+
+// #[derive(Serialize, Deserialize)]
+pub struct User {
+    password: String
+}
+
+impl User {
+    pub fn get_password(&self) -> &String {
+        return &self.password;
+    }
+}
