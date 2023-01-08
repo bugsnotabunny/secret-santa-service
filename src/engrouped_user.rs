5bbc566bb500892e4cf7badde659040a16f821e3
BVNkib <bazzhinv@gmail.com>
2023-01-08 20:15:24 +0300

    rename permission and add setter for it

--- src/engrouped_user.rs
+++ src/engrouped_user.rs
@@ -2,13 +2,13 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct EngroupedUser {
-    permission: bool,
+    is_admin: bool,
     login: String,
     receivers_login: String
 }
 impl EngroupedUser {
-    pub fn get_permission(self) -> bool{
-        self.permission
+    pub fn get_is_admin(&self) -> bool{
+        self.is_admin
     }
 
     pub fn get_login(&self) -> &String{
@@ -17,5 +17,9 @@ impl EngroupedUser {
 
     pub fn get_recievers_login(&self) -> &String{
         &self.receivers_login
+    }
+
+    pub fn set_is_admin(&mut self, is_admin_: bool){
+        self.is_admin = is_admin_;
     }
 }
