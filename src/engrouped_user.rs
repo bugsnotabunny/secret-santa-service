886135c6da416e7affba069f2458eaaa3618c151
BVNkib <bazzhinv@gmail.com>
2023-01-08 21:07:07 +0300

    add new with originally set receivers login

--- src/engrouped_user.rs
+++ src/engrouped_user.rs
@@ -3,16 +3,15 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 pub struct EngroupedUser {
     is_admin: bool,
-    login: String,
     receivers_login: String
 }
 impl EngroupedUser {
+    pub fn new(is_admin_: bool) -> EngroupedUser{
+        let recivers_login_ = String::from("");
+        EngroupedUser {is_admin: is_admin_, receivers_login: recivers_login_}
+    }
     pub fn get_is_admin(&self) -> bool{
         self.is_admin
-    }
-
-    pub fn get_login(&self) -> &String{
-        &self.login
     }
 
     pub fn get_recievers_login(&self) -> &String{
